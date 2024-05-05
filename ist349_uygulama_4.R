d41f453eb80918e38119c09b3c68ec6fea2c056f
IsmailYenilmez <73519591+IsmailYenilmez@users.noreply.github.com>
2024-05-05 18:04:07 +0300

    Add files via upload

--- ist349_uygulama_4.R
+++ ist349_uygulama_4.R
@@ -13,3 +13,29 @@ lpm_model <- lm(purchase ~ income, data = data)
 
 # Summary of the model
 summary(lpm_model)
+
+
+# logit: Generate simulated data
+set.seed(123)
+n <- 1000  # Number of observations
+x1 <- rnorm(n)  # Predictor variable 1
+x2 <- rnorm(n)  # Predictor variable 2
+p <- 0.3 + 0.5 * x1 - 0.8 * x2  # True relationship (logit function)
+y <- rbinom(n, 1, plogis(p))  # Simulate binary outcome based on logit function
+
+# Create a data frame
+data_logit <- data.frame(y, x1, x2)
+
+# Fit Logit Model
+logit_model <- glm(y ~ x1 + x2, data = data_logit, family = binomial(link = "logit"))
+
+# Summary of the model
+summary(logit_model)
+
+
+# probit: Fit Probit Model
+probit_model <- glm(y ~ x1 + x2, data = data_logit, family = binomial(link = "probit"))
+
+# Summary of the model
+summary(probit_model)
+
